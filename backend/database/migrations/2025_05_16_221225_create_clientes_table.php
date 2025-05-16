fc1619c68767a3cc6d4c2a249776e79a9b54a0b2
KauaRodrigo <63003237+KauaRodrigo@users.noreply.github.com>
2025-05-16 19:25:19 -0300

    Estrutura inicial laravel

---
+++ backend/database/migrations/2025_05_16_221225_create_clientes_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id('clicodigo');
+            $table->string('clinome', 100);
+            $table->string('clicpf', 11);
+            $table->date('clidata_nascimento');
+            $table->string('clitelefone', 11)->nullable();
+            $table->text('cliendereco')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::dropIfExists('clientes');
+    }
+};
