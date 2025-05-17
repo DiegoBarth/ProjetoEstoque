4d75eb2e87c2ae47151d835259ad3aa058a8373b
KauaRodrigo <63003237+KauaRodrigo@users.noreply.github.com>
2025-05-17 00:11:18 -0300

    Configuração inicial do front

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,10 +1,17 @@
-<?php
+<?
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
-    'allowed_headers' => ['*'],
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'web/*'], // adiciona as rotas que você tá usando        
     'exposed_headers' => [],
+    'paths' => ['*'],
+    'allowed_origins' => ['*'],
+    'allowed_methods' => ['*'],
+    'allowed_headers' => ['*'],
+
     'max_age' => 0,
+
+    'supports_credentials' => true,
+
 ];
+
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import api from '../api';
+
+async function chamaApi() {
+  await api.get('/api');
+}
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <button @click="chamaApi()">Clica aqui pai</button>
+  </main>
+</template>
