b03753bd846fd4ea8cad7a3151651664128a286e
DiegoBarth <d.alexbarth@gmail.com>
2025-05-22 19:05:56 -0300

    Ajustes na tela de login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,21 +1,26 @@
 <template>
    <div class="flex w-full h-full">
       <div class="cardLogin z-1 card bg-white w-2/3 h-full items-center justify-center flex">
-         <div class="card bg-white shadow-xl h-1/2 w-2/4 items-center justify-center flex flex-col rounded-2xl" style="box-shadow: 0px 0px 50px lightgray">
-               <div class="flex items-center gap-5 py-7">
-                  <img src="../assets/img/logo1.png" class="h-29" alt="">
-                  <i class="fa fa-plus text-4xl"></i>
-                  <img src="../assets/img/logo2.png" class="h-29" alt="">
-               </div>                
-               <div class="flex flex-col gap-2">
-                  <input class="border-1 rounded-sm w-xs p-2" style="border-color: #E0E0E0; outline: none;" type="text" v-model="sUsuario" name="usuario" placeholder="Usuário"/>
-                  <div class="relative">
-                     <input class="border-1 rounded-sm w-xs p-2" id="senha" style="border-color: #E0E0E0; outline: none;" type="password" v-model="sSsenha" name="senha" placeholder="Senha"/>
-                     <i class="fa fa-eye absolute top-3.5 right-3 cursor-pointer" @click="alteraCampoSenha"></i>
-                  </div>
-                  <p style="color: rgb(248, 70, 70)">{{sErro}}</p>
-                  <button @click="login" type="submit" style="background-color: #7ac1e3; font-weight: 600" class="cursor-pointer w-1/3 rounded p-1 text-white">Entrar</button>
+         <div class="bg-white shadow-xl h-auto max-h-screen w-[90%] max-w-[320px] sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto flex flex-col items-center justify-center rounded-2xl p-4 sm:p-6" style="box-shadow: 0px 0px 50px lightgray">
+            <div class="w-full">
+               <div class="flex flex-col sm:flex-row items-center justify-center gap-5 lg:gap-10 py-7 w-full px-4">
+                  <img src="../assets/img/logo1.png" class="h-20 sm:h-24 md:h-28 w-auto max-w-[80%] sm:max-w-[40%]" alt="" />
+                  <img src="../assets/img/logo2.png" class="h-20 sm:h-24 md:h-28 w-auto max-w-[80%] sm:max-w-[40%]" alt="" />
                </div>
+               <form @submit.prevent="login" class="flex flex-col items-center w-full px-4 sm:px-6">
+                  <div class="flex flex-col gap-3 w-2/3 max-w-xs sm:max-w-sm md:max-w-md mx-auto">
+                     <input class="border rounded p-2 w-full max-w-[280px]" style="border-color: #E0E0E0; outline: none;" type="text" v-model="sUsuario" name="usuario" placeholder="Usuário"/>
+                     <div class="relative w-full max-w-[280px]">
+                        <input class="border rounded p-2 w-full" id="senha" style="border-color: #E0E0E0; outline: none;" type="password" v-model="sSsenha" name="senha" placeholder="Senha"/>
+                        <i class="fa fa-eye absolute top-3.5 right-3 cursor-pointer" @click="alterarCampoSenha"></i>
+                     </div>
+                     <p style="color: rgb(248, 70, 70); text-align: center;">{{ sErro }}</p>
+                     <button type="submit" style="background-color: #7ac1e3; font-weight: 600" class="cursor-pointer bg-sky-400 text-white font-semibold rounded p-2 w-full sm:w-30 mt-2">
+                        Entrar
+                     </button>
+                  </div>
+               </form>
+            </div>
          </div>
       </div>        
       <img src="../assets/img/fundoLogin.png" class="w-1/3 z-0">
@@ -32,15 +37,21 @@
    const sErro    = ref('');
    const oRouter  = useRouter();
 
-   function alteraCampoSenha() {
+   function alterarCampoSenha() {
       if($('#senha').prop('type') == 'text') {
          return $('#senha').prop('type', 'password');
       }
+      
       $('#senha').prop('type', 'text');
    }
 
    async function login() {
       sErro.value = '';
+
+      if(!sUsuario.value || !sSsenha.value) {
+         sErro.value = 'Usuário e senha são obrigatórios.';
+         return;
+      }
 
       utils.adicionarLoading();
 
