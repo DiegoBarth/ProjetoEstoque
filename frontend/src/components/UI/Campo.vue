ccd0b7f6066d02dda9f5dd70ad12fe4424303eff
KauaRodrigo <63003237+KauaRodrigo@users.noreply.github.com>
2025-05-22 21:49:06 -0300

    visualização e exclusão de produtos (modal de exclusão só para testes)

--- frontend/src/components/UI/Campo.vue
+++ frontend/src/components/UI/Campo.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <label :class="{'obrigatorio': bObrigatorio}" class="block text-sm font-medium">{{ sTitulo }}</label>
-        <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" v-if="sTipo != 'select'" v-bind="$attrs" :type="oCampo.sTipo" class="w-full mt-1 p-2 border rounded" :placeholder="oCampo.sPlaceholder" :required="oCampo.bObrigatorio"/>
-        <select v-else @change="$emit('update:modelValue', $event.target.value)" v-bind="$attrs" :value="modelValue" class="w-full mt-1 p-2 border rounded" :placeholder="oCampo.sPlaceholder" :required="oCampo.bObrigatorio">
+        <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" v-if="sTipo != 'select'" v-bind="$attrs" :type="oCampo.sTipo" class="w-full mt-1 p-2 border rounded disabled:bg-gray-200" :placeholder="oCampo.sPlaceholder" :required="oCampo.bObrigatorio"/>
+        <select v-else @change="$emit('update:modelValue', $event.target.value)" v-bind="$attrs" :value="modelValue" class="w-full mt-1 p-2 border rounded disabled:bg-gray-200" :placeholder="oCampo.sPlaceholder" :required="oCampo.bObrigatorio">
             <option value="">Selecione</option>
             <option v-for="(oOpcao, iIndice) of aOpcoes" :key="iIndice" :value="oOpcao.iValor">
                 {{ oOpcao.sDescricao }}
