a1f4c542beebbdd7ecba323a661007812bb95230
DiegoBarth <d.alexbarth@gmail.com>
2025-05-24 16:51:00 -0300

    Ajustes layout login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,23 +1,26 @@
 <template>
    <div class="flex w-full h-full">
       <div class="cardLogin z-1 card bg-white w-2/3 h-full items-center justify-center flex">
-         <div class="bg-white shadow-xl h-auto max-h-screen w-[90%] max-w-[320px] sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto flex flex-col items-center justify-center rounded-2xl p-4 sm:p-6" style="box-shadow: 0px 0px 50px lightgray">
+ <div class="card-principal shadow-xl min-h-[70vh] w-[90%] max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto flex flex-col items-center justify-center rounded-2xl p-4 sm:p-6">
             <div class="w-full">
                <div class="flex flex-col sm:flex-row items-center justify-center gap-5 lg:gap-10 py-7 w-full px-4">
-                  <img src="../assets/img/logo1.png" class="h-20 sm:h-24 md:h-28 w-auto max-w-[80%] sm:max-w-[40%]" alt="" />
-                  <img src="../assets/img/logo2.png" class="h-20 sm:h-24 md:h-28 w-auto max-w-[80%] sm:max-w-[40%]" alt="" />
+                  <a href="https://www.instagram.com/ifix_foryou" target="_blank" rel="noopener noreferrer">
+                     <img src="../assets/img/logo1.png" alt="" class="h-20 sm:h-24 md:h-28  cursor-pointer transition-transform duration-300 ease-in-out hover:scale-95"/>
+                  </a>
+                  <a href="https://www.instagram.com/foryou__tech" target="_blank" rel="noopener noreferrer">
+                     <img src="../assets/img/logo2.png" alt="" class="h-20 sm:h-24 md:h-28  cursor-pointer transition-transform duration-300 ease-in-out hover:scale-95"/>
+                  </a>
                </div>
                <form @submit.prevent="login" class="flex flex-col items-center w-full px-4 sm:px-6">
+                  <div class="texto relative w-full max-w-[280px]" style="text-align: center; height:70px; font-size:30px; font-weight: 500;">Acesso ao Sistema</div>
                   <div class="flex flex-col gap-3 w-2/3 max-w-xs sm:max-w-sm md:max-w-md mx-auto">
-                     <input class="border rounded p-2 w-full max-w-[280px]" style="border-color: #E0E0E0; outline: none;" type="text" v-model="sUsuario" name="usuario" placeholder="Usuário"/>
+                     <Input sTipo="text" v-model="sUsuario" name="usuario" sPlaceholder="Usuário"/>
                      <div class="relative w-full max-w-[280px]">
-                        <input class="border rounded p-2 w-full" id="senha" style="border-color: #E0E0E0; outline: none;" type="password" v-model="sSsenha" name="senha" placeholder="Senha"/>
-                        <i class="fa fa-eye absolute top-3.5 right-3 cursor-pointer" @click="alterarCampoSenha"></i>
+                        <Input sTipo="password" v-model="sSenha" id="senha" name="senha" sPlaceholder="Senha"/>
+                        <i class="fa fa-eye absolute top-3.5 right-3 cursor-pointer" style="color: #4D4D4D;" @click="alterarCampoSenha"></i>
                      </div>
                      <p style="color: rgb(248, 70, 70); text-align: center;">{{ sErro }}</p>
-                     <button type="submit" style="background-color: #7ac1e3; font-weight: 600" class="cursor-pointer bg-sky-400 text-white font-semibold rounded p-2 w-full sm:w-30 mt-2">
-                        Entrar
-                     </button>
+                     <Botao sStyle="font-weight: 600;align-self: center;" sTexto="Entrar" sTipo="submit" sId="button_login" sLargura="w-full sm:w-40"/>
                   </div>
                </form>
             </div>
@@ -27,39 +30,32 @@
    </div>
 </template>
 <script setup>
-   import { useRouter } from 'vue-router';
-   import { ref } from 'vue';
    import api from '../api';
+   import { ref } from 'vue';
+   import { useRouter } from 'vue-router';
+   import { useUsuarioStore } from '../stores/usuarioStore';
    import Cookies from 'js-cookie';
+   import Input from '../components/UI/Input.vue';
+   import Botao from '../components/UI/Botao.vue';
 
-   const sUsuario = ref('');
-   const sSsenha  = ref('');
-   const sErro    = ref('');
-   const oRouter  = useRouter();
-
-   function alterarCampoSenha() {
-      if($('#senha').prop('type') == 'text') {
-         return $('#senha').prop('type', 'password');
-      }
-      
-      $('#senha').prop('type', 'text');
-   }
+   const sUsuario      = ref('');
+   const sSenha        = ref('');
+   const sErro         = ref('');
+   const oRouter       = useRouter();
+   const oUsuarioStore = useUsuarioStore();
 
    async function login() {
       sErro.value = '';
 
-      if(!sUsuario.value || !sSsenha.value) {
+      if(!sUsuario.value || !sSenha.value) {
          sErro.value = 'Usuário e senha são obrigatórios.';
          return;
       }
 
       try {
-         await api.get('/sanctum/csrf-cookie')
+         await oUsuarioStore.getToken();
 
-         const {data, status} = await api.post('/api/login', {
-            usunome_usuario: sUsuario.value,
-            password       : sSsenha.value
-         });         
+         const {data, status} = await oUsuarioStore.login(sUsuario.value, sSenha.value);
 
          if(status != 200) {
             return sErro.value = data.sMensagem;
@@ -76,6 +72,14 @@
       }
    }
 
+   function alterarCampoSenha() {
+      if($('#senha').prop('type') == 'text') {
+         return $('#senha').prop('type', 'password');
+      }
+      
+      $('#senha').prop('type', 'text');
+   }
+
    function setDadosCookies(oUsuario) {
       Cookies.set('iUsuario', oUsuario.usucodigo);
       Cookies.set('sUsuario', oUsuario.usunome);
@@ -84,5 +88,6 @@
 <style scoped>
 .cardLogin {
    box-shadow: 10px 0px 100px rgb(19, 19, 19);
+   background-color: #E9EFF2;
 }
 </style>
