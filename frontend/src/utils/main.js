519e59cdff7f2d1fac2785578672015abaf77dd4
DiegoBarth <d.alexbarth@gmail.com>
2025-05-23 23:31:25 -0300

    Formatador de CPF e telefone

--- frontend/src/utils/main.js
+++ frontend/src/utils/main.js
@@ -34,3 +34,27 @@ export function adicionarLoading() {
 export function removerLoading() { 
    document.getElementById('loading_sistema').classList.remove('active');
 }
+
+export function formatarCPF(oElemento) {
+   let sCpf = oElemento.value;
+
+   sCpf = sCpf.replace(/\D/g, '');
+   sCpf = sCpf.replace(/(\d{3})(\d)/, '$1.$2');
+   sCpf = sCpf.replace(/(\d{3})(\d)/, '$1.$2');
+   sCpf = sCpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+
+   return oElemento.value = sCpf;
+}
+
+export function  formatarTelefone(sTelefone) {
+  sTelefone = sTelefone.replace(/\D/g, '');
+
+   if(sTelefone.length <= 10) {
+      sTelefone = sTelefone.replace(/^(\d{2})(\d{4})(\d{0,4})$/, '($1) $2-$3');
+   }
+   else {
+      sTelefone = sTelefone.replace(/^(\d{2})(\d{5})(\d{0,4})$/, '($1) $2-$3');
+   }
+
+  return sTelefone.trim();
+}
