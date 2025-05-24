04f400fb1c38036de37798a0a1989cd5e510f994
DiegoBarth <d.alexbarth@gmail.com>
2025-05-24 10:36:04 -0300

    Adicionada busca por CPF na store de cliente

--- frontend/src/stores/clienteStore.js
+++ frontend/src/stores/clienteStore.js
@@ -2,19 +2,39 @@ import api from "@/api";
 import { defineStore } from "pinia";
 
 export const useClienteStore = defineStore('Cliente', {
-    state: () => ({
+   state: () => ({
 
-    }),
-    actions: {
-        getClientes: async () => {
-            try {
-                const { data } = await api.get('/api/cliente');
+   }),
+   actions: {
+      _formatarCliente(oCliente) {
+         return {
+            iCliente       : oCliente.clicodigo,
+            sNome          : oCliente.clinome,
+            sCpf           : utils.formatarCPF(oCliente.clicpf),
+            sDataNascimento: utils.formatarDataBR(oCliente.clidata_nascimento),
+            sEndereco      : oCliente.cliendereco,
+            sTelefone      : utils.formatarTelefone(oCliente.clitelefone)
+         };
+      },
+      async getClientes() {
+         try {
+            const { data } = await api.get('/api/cliente');
 
-                return data.aClientes;
-            }
-            catch(error) {
-                throw(error);
-            }
-        }
-    }
-}) 
+            return data.aClientes;
+         }
+         catch (error) {
+            throw (error);
+         }
+      },
+      async getClienteByCPF(sValorCpf) {
+         try {
+            const { data } = await api.get(`/api/cliente/busca/cpf?cpf=${encodeURIComponent(sValorCpf)}`);
+
+            return this._formatarCliente(data.oCliente);
+         }
+         catch (error) {
+            throw (error);
+         }
+      }
+   }
+}); 
