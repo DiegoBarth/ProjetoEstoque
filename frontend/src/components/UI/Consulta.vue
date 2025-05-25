7153b65638602f1de3975670d4fe145c188465d0
DiegoBarth <d.alexbarth@gmail.com>
2025-05-25 14:33:07 -0300

    Alterado para que o botão padrão da tela de consulta use o componente Botao

--- frontend/src/components/UI/Consulta.vue
+++ frontend/src/components/UI/Consulta.vue
@@ -1,19 +1,19 @@
 <template>
-    <div>
-        <div class="flex items-center justify-between w-full py-10 px-30">
-        <h1 class="cabecalho-principal text-3xl text-white font-semibold ">{{ sTitulo }}</h1>
-        <button @click="$emit('showModalCadastro')" class="cursor-pointer rounded-sm p-2 bg-[var(--principal)] text-white">Cadastrar {{ sTitulo.toLowerCase() }}</button>
-        </div>
-        <div class="flex flex-wrap w-full gap-5 px-30">
-            <slot name="filtros"></slot>
-        </div>
-        <slot name="gridConsulta"></slot>
-    </div>
+   <div>
+      <div class="flex items-center justify-between w-full py-10 px-30">
+         <h1 class="cabecalho-principal text-3xl text-white font-semibold ">{{ sTitulo }}</h1>
+         <Botao @click="$emit('showModalCadastro')" :sTexto="`Cadastrar ${sTitulo.toLowerCase()}`" sTipo="text" sId="botao_cadastrar" sLargura="w-fit" />
+      </div>
+      <div class="flex flex-wrap w-full gap-5 px-30">
+         <slot name="filtros"></slot>
+      </div>
+      <slot name="gridConsulta"></slot>
+   </div>
 </template>
 <script setup>
-defineProps(['sTitulo'])
-defineEmits(['showModalCadastro'])
-</script>
-<style lang="scss"scoped>
+import Botao from '../UI/Botao.vue';
 
-</style>
+defineProps(['sTitulo']);
+defineEmits(['showModalCadastro']);
+</script>
+<style lang="scss" scoped></style>
