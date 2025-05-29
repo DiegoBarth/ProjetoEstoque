663228aea7b2ca4734e8414caf385bcdb2d4e8f9
KauaRodrigo <63003237+KauaRodrigo@users.noreply.github.com>
2025-05-28 21:37:13 -0300

    Removido slot para filtros e adicionado parâmetro para definir se o botão de cadastro será apresentado

--- frontend/src/components/UI/Consulta.vue
+++ frontend/src/components/UI/Consulta.vue
@@ -2,18 +2,15 @@
    <div>
       <div class="flex items-center justify-between w-full py-10 px-30">
          <h1 class="cabecalho-principal text-3xl text-white font-semibold ">{{ sTitulo }}</h1>
-         <Botao @click="$emit('showModalCadastro')" :sTexto="`Cadastrar ${sTitulo.toLowerCase()}`" sTipo="text" sId="botao_cadastrar" sLargura="w-fit" />
-      </div>
-      <div class="flex flex-wrap w-full gap-5 px-30">
-         <slot name="filtros"></slot>
-      </div>
+         <Botao v-if="bMostraBotao" @click="$emit('showModalCadastro')" :sTexto="`Cadastrar ${sTitulo.toLowerCase()}`" sTipo="text" sId="botao_cadastrar" sLargura="w-fit" />
+      </div>      
       <slot name="gridConsulta"></slot>
    </div>
 </template>
 <script setup>
 import Botao from '../UI/Botao.vue';
 
-defineProps(['sTitulo']);
+defineProps(['sTitulo', 'bMostraBotao']);
 defineEmits(['showModalCadastro']);
 </script>
 <style lang="scss" scoped></style>
