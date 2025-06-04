f4a86bc9778a821b1e2754fc3a708bc1bd0b140b
KauaRodrigo <63003237+KauaRodrigo@users.noreply.github.com>
2025-06-04 20:38:07 -0300

    alterações no layout do modal de devoluções

--- frontend/src/components/UI/Grid.vue
+++ frontend/src/components/UI/Grid.vue
@@ -1,5 +1,5 @@
 <template>
-   <table class="shadow-md mx-auto rounded-sm" id="tabela" style="width: 86%;">
+   <table class="shadow-md mx-auto rounded-sm w-full" id="tabela">
       <thead class="bg-black rounded-t-sm sticky top-0">
          <tr>
             <th v-for="(sCabecalho, iIndice) of aCabecalhos" class="p-2 text-left text-white" :key="iIndice">{{
@@ -8,29 +8,44 @@
       </thead>
       <tbody v-bind="$attrs">
          <slot></slot>
-      </tbody>
+      </tbody>      
    </table>
 </template>
 <script setup>
 import { nextTick, onMounted } from "vue";
 import 'datatables.net-dt';
 
-const oPropriedades = defineProps(['aCabecalhos', 'sLayout']);
+const oPropriedades = defineProps({
+  aCabecalhos: {
+    type: Array,
+    required: true
+  },
+  sLayout: {
+    type: String,
+    required: false
+  },
+  bDataTable: {
+    type: Boolean,
+    default: true
+  }
+});
 
 onMounted(() => {
-   nextTick(() => {
-      $('#tabela').DataTable({
-         pageLength: 10,
-         responsive: false,
-         order: [[0, 'asc']],
-         columnDefs: [{
-            orderable: false, targets: -1
-         }],
-         language: {
-            url: "/js/datatable/pt-BR.json"
-         }
+   if(oPropriedades.bDataTable) {
+      nextTick(() => {
+         $('#tabela').DataTable({
+            pageLength: 10,
+            responsive: false,
+            order: [[0, 'asc']],
+            columnDefs: [{
+               orderable: false, targets: -1
+            }],
+            language: {
+               url: "/js/datatable/pt-BR.json"
+            }
+         });
       });
-   });
+   }
 });
 </script>
 <style lang="scss" scoped></style>
